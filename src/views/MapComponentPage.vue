05e81cd2f501331185db4690768befb1d4a99739
Krist <cysh110254@cy-tech.com>
2024-07-23 11:31:05 +0800

    2024/07/23 - Adjust - 修改地圖出錯問題 by Krist

--- src/views/MapComponentPage.vue
+++ src/views/MapComponentPage.vue
@@ -13,73 +13,122 @@
         </div>
       </div> -->
 
-      <button class="map-button btn btn-danger" @click="addTileLayer">新增圖層</button>
+      <button class="map-button btn btn-danger" @click="addTileLayer">新增國土圖層</button>
       
-      <button class="map-button-1 btn btn-danger" @click="recoverTileLayer">復原圖層</button>
+      <button class="map-button-1 btn btn-danger" @click="addAtmosphereTileLayer">新增氣象圖層</button>
     </div>
   </template>
-  
-  <script>
-  import { onMounted, ref } from 'vue';
-  import L from 'leaflet';
-  import 'leaflet/dist/leaflet.css';
-  
-  export default {
-    name: 'MapComponentPage',
-    setup() {
-      const map = ref(null);
-      let tileLayer = null; // 定義圖塊圖層變數
-  
-      onMounted(() => {
-        // 初始化地圖
-        map.value = L.map('map').setView([24.91, 121.673], 13); // 注意：緯度在前，經度在後
-  
-        // 設置地圖圖塊圖層
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map.value);
-  
-        // 添加一個標記
-        L.marker([24.91, 121.673]).addTo(map.value)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-          .openPopup();
-      });
-  
-      // 新增圖塊圖層方法
-      const addTileLayer = () => {
-        if (tileLayer) {
-          map.value.removeLayer(tileLayer); // 如果圖塊圖層已存在，先移除
-        }
-  
-        // 使用國土測繪中心地圖服務
-        tileLayer = L.tileLayer('https://wmts.nlsc.gov.tw/wmts/PHOTO2/default/GoogleMapsCompatible/{z}/{y}/{x}', {
-          attribution: '國土測繪中心',
-          maxZoom: 18,
-        }).addTo(map.value);
-      };
-  
-      // 新增氣象局雷達回波地圖
-      const addAtmosphereTileLayer = () => {
-        if (tileLayer) {
-          map.value.removeLayer(tileLayer);
-        }
-  
-        // 使用中央氣象局雷達回波地圖服務
-        tileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-          attribution: 'Tiles &copy; Esri &mdash; Source: Esri',
-          maxZoom: 16,
-        }).addTo(map.value);
+
+<script>
+import { onMounted, ref } from 'vue';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+  name: 'MapComponentPage',
+  setup() {
+    const map = ref(null);
+    const currentTileLayer = ref(null); // 跟蹤當前的圖塊圖層
+
+    onMounted(() => {
+      initMap();
+    });
+
+    const initMap = () => {
+      // 初始化地圖
+      map.value = L.map('map').setView([24.91, 121.673], 13);
+
+      // 設置默認的地圖圖塊圖層
+      const defaultTileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map.value);
+
+      // 添加一個標記
+      // L.marker([24.91, 121.673]).addTo(map.value)
+      //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      //   .openPopup();
+
+      // 將默認圖塊圖層設置為當前圖塊圖層
+      currentTileLayer.value = defaultTileLayer;
+    };
+
+    const addTileLayer = () => {
+      if (map.value && currentTileLayer.value) {
+        map.value.removeLayer(currentTileLayer.value); // 移除當前圖塊圖層
       }
 
-  
-      return {
-        addTileLayer,
-        addAtmosphereTileLayer,
-      };
-    }
-  };
-  </script>
+         // 地圖在準備好之後才進行縮放
+         if (map.value) {
+          map.value.whenReady(() => {
+          map.value.setView([24.91, 121.673], 13);
+        });
+      } else {
+        map.value.setView([24.91, 121.673], 13);
+      }
+
+      // 使用國土測繪中心地圖服務
+      const tileLayer1 = L.tileLayer('https://wmts.nlsc.gov.tw/wmts/PHOTO2/default/GoogleMapsCompatible/{z}/{y}/{x}', {
+        attribution: '國土測繪中心',
+        maxZoom: 19,
+      }).addTo(map.value);
+
+      // 更新當前圖塊圖層
+      // currentTileLayer.value = tileLayer1;
+
+       // 地圖在準備好之後才進行縮放
+       if (map.value) {
+        map.value.whenReady(() => {
+          map.value.setView([24.91, 121.673], 13);
+          currentTileLayer.value = tileLayer1;
+        });
+      } else {
+        map.value.setView([24.91, 121.673], 13);
+      }
+    };
+
+    const addAtmosphereTileLayer = () => {
+      if (map.value && currentTileLayer.value) {
+        map.value.removeLayer(currentTileLayer.value); // 移除當前圖塊圖層
+      }
+
+         // 地圖在準備好之後才進行縮放
+         if (map.value) {
+        map.value.whenReady(() => {
+          map.value.setView([24.91, 121.673], 13);
+        });
+      } else {
+        map.value.setView([24.91, 121.673], 13);
+      }
+
+      // 使用中央氣象局雷達回波地圖服務
+      const tileLayer2 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri',
+        maxZoom: 19,
+      }).addTo(map.value);
+
+      // 更新當前圖塊圖層
+      // currentTileLayer.value = tileLayer2;
+
+       // 地圖在準備好之後才進行縮放
+       if (map.value) {
+        map.value.whenReady(() => {
+          map.value.setView([24.91, 121.673], 13);
+          currentTileLayer.value = tileLayer2;
+        });
+      } else {
+        map.value.setView([24.91, 121.673], 13);
+      }
+    };
+
+    return {
+      initMap,
+      addTileLayer,
+      addAtmosphereTileLayer,
+    };
+  }
+};
+</script>
   
   <style>
   #map {
