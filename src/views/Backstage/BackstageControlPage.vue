9230703e8b828015d9c4891e8ea38001aac8a97b
Krist <cysh110254@cy-tech.com>
2024-08-14 14:59:22 +0800

    2024/08/14 - Feature - 新增創建表單功能 by Krist

--- src/views/Backstage/BackstageControlPage.vue
+++ src/views/Backstage/BackstageControlPage.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
       <h1>後臺管理系統</h1>
+
+      <div class="container">
+        <div class="row">
+            <div class="col-10"></div>
+            <div class="col-2">
+                <button class="btn btn-success col-" @click="openCreateModal">新增表單</button>
+            </div>
+        </div>
+      </div>
+
       <table class="table">
         <thead>
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Name</th>
                 <th scope="col">Phone</th>
+                <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -14,6 +25,10 @@
                 <td>{{ item.id }}</td>
                 <td>{{ item.name }}</td>
                 <td>{{ item.phone }}</td>
+                <td>
+                    <button class="btn btn-primary me-2" @click="editItem(item)">Edit</button>
+                    <button class="btn btn-danger" @click="deleteItem(item,id)">Delete</button>
+                </td>
             </tr>
         </tbody>
       </table>
@@ -22,12 +37,18 @@
   
   <script>
   import axiosInstance from '@/axios.js';
+  import Swal from 'sweetalert2';
+
   export default {
     name: 'BackstageControl',
 
     data() {
         return {
             items: [], // 負責存放後端獲取的資料
+            currentItem: {
+                name: '',
+                phone: ''
+            }
         }
     },
 
@@ -37,6 +58,7 @@
 
     methods: {
     
+        // 取得所有資料列表
         async getItem() {
             try {
                 const response = await axiosInstance.get("https://localhost:7243/api/Backstage");
@@ -45,9 +67,69 @@
             } catch (error) {
                 console.log("Error fetching items", error);
             }
-        }
-    },
+        },
 
+        // 處理表單提交
+        async handleSubmit() {
+            try{
+                await this.createItem();
+                this.getItem(); // 更新清單
+                Swal.close();   // 關閉彈跳視窗
+            } catch (error){
+                console.error('Error handling from submit:', error );
+            }
+        },
+
+        // 新增進入後端
+        async createItem() {
+            try{
+                console.log(this.currentItem)
+                await axiosInstance.post("https://localhost:7243/api/Backstage/CreateItem", this.currentItem);
+            } catch(error) {
+                console.error('Error creating item', error);
+            }
+        },
+
+        // 開啟創建表單
+        openCreateModal() {
+            this.resetForm();
+            this.showModel('Create New Item');
+        },
+        
+        // 重製表單
+        resetForm() {
+            this.currentItem = {
+                name: '',
+                phone: ''
+            }
+        },
+
+        // 打開表單內容
+        showModel(title) {
+            Swal.fire({
+                title,
+                html: `
+                    <div>
+                        <label for="name">Name:</label>
+                        <input id="name" type="text" class="swal2-input" v-model="currentItem.name" />
+                    </div>
+                    <div>
+                        <label for="phone">Phone:</label>
+                        <input id="phone" type="phone" class="swal2-input" v-model="currentItem.price" />
+                    </div>
+                `,
+                focusConfirm: false,
+                preConfirm: () => {
+                    this.currentItem.name = document.getElementById('name').value;
+                    this.currentItem.phone = document.getElementById('phone').value;
+                    this.handleSubmit();
+                }
+            })
+        }
+
+
+    },
+    // 創建表單
     created() {
       console.log('ContactPage created');
     }
