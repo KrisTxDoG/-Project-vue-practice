f66367cdc9dc0be76290b441f08de2853b7d6c2d
Krist <cysh110254@cy-tech.com>
2024-08-15 10:15:36 +0800

    2024/08/15 - Feature - 新增上傳 Base64 存入資料庫功能 by Krist

--- src/views/FileUploadPage.vue
+++ src/views/FileUploadPage.vue
@@ -13,6 +13,15 @@
                 <button class="btn btn-primary" @click="uploadImage">Upload</button>
             </div>
         </div>
+
+        <div class="row mt-4">
+            <div class="col-6 h3">請上傳資料 (轉為 Base64)</div>
+            <!-- 使用 input-group 將 input 和 button 組成群組 -->
+            <div class="col-10 input-group">
+                <input class="form-control" type="file" @change="handleFileChange" />
+                <button class="btn btn-primary" @click="handleFileUpload">Upload</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -32,6 +41,41 @@
         methods: {
             handleFileChange(event) {
             this.selectedFile = event.target.files[0];
+        },
+
+        // 處理上傳 Base64 的檔案
+        handleFileUpload() {
+            // const file = event.target.files[0];
+            if (!this.selectedFile) {
+                alert('Please select a file first.');
+                return;
+            }
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+                const base64String = e.target.result.split(',')[1]; // 只取 base64 的部分
+                this.uploadFile(base64String);
+            };
+
+            reader.readAsDataURL(this.selectedFile); // 讀取檔案為 Data URL
+        },
+
+        uploadFile(base64String) {
+            // 假設你有一個上傳檔案的 API
+            axiosInstance.post('https://localhost:7243/api/Upload/UploadFileBase64', {
+                fileName: this.selectedFile.name,
+                contentType: 'NONO',
+                fileDate: base64String,
+            })
+            .then(response => {
+                // 處理成功回應
+                console.log('File uploaded successfully', response);
+                this.uploadedFileName = this.selectedFile.name;
+            })
+            .catch(error => {
+                // 處理錯誤
+                console.error('Error uploading file', error);
+            });
         },
 
         async uploadImage() {
